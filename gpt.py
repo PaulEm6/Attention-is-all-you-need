c19119b61840412a9457b08ac295882979b29829
PaulEm6 <paulemmanuelatte12@gmail.com>
2024-02-24 19:53:08 -0500

    added input and output preparation of simple gpt about to start work on neural network architecture

--- gpt.py
+++ gpt.py
@@ -25,6 +25,7 @@ with open('input.txt', 'r', encoding='utf-8') as f:
 # here are all the unique characters that occur in this text
 chars = sorted(list(set(text)))
 vocab_size = len(chars)
+
 # create a mapping from characters to integers
 stoi = { ch:i for i,ch in enumerate(chars) }
 itos = { i:ch for i,ch in enumerate(chars) }
--- simple_gpt.py
+++ simple_gpt.py
@@ -2,17 +2,77 @@
 
 import torch
 import torch.nn as nn
+from torch.nn import functional as F
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-
-#Hyperparameters
-vocab_size = 64 #number of different tokens in dictionary
-block_size = 8 #maximum number of tokens to be considered as one "input"
+'''Hyperparameters'''
+batch_size = 64 # how many independent sequences will we process in parallel?
+block_size = 256 #maximum number of tokens to be considered as "input" for predictions
 n_embed = 32 #dimension of vector after embedding
+max_iters = 10
+eval_interval = 1
+learning_rate = 3e-4
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+eval_iters = 200
+n_head = 6
+n_layer = 6
+dropout = 0.2
+#Preparing Environment for training
+torch.manual_seed(1337)
 
-class BigramLanguageModel(nn.Module):
+# wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
+with open('input.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+
+# here are all the unique characters that occur in this text
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+
+'''Preparing Input'''
+# create a mapping from characters to integers
+stoi = { ch:i for i,ch in enumerate(chars) }
+itos = { i:ch for i,ch in enumerate(chars) }
+encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
+decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
+
+# Train and test splits
+data = torch.tensor(encode(text), dtype=torch.long)
+n = int(0.9*len(data)) # first 90% will be train, rest val
+train_data = data[:n]
+val_data = data[n:]
+
+# data loading
+def get_batch(split):
+    # generate a small batch of data of inputs x and targets y
+    # Maps inputs x and targets y in an autoregerssive manner for each block in the batch
+        # Token position 0 has an output of token position 1; # Tokens position 0 to 1 have an output of token position 2;
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    x, y = x.to(device), y.to(device)
+    return x, y
+
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+'''Preparing Output'''
+
+
+class LanguageModel(nn.Module):
     def __init__(self):
-        super.__init__()
+        super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, n_embed)
         self.position_embedding_table = nn.Embedding(block_size, n_embed)
 
@@ -22,6 +82,26 @@ class BigramLanguageModel(nn.Module):
         positional_embedding = self.token_embedding_table(torch.arange(T, device=device))
         x = token_embedding + positional_embedding
         return x
+    
+    def generate(self, idx, max_new_tokens):
+    #For each block within the batch, this method generates the most probable token(s) to come next
+    #For simple use, pass a batch of only one block to this method in order to simulate "ChatGPT" like interaction of "prompt" and "answer"
+        # idx is (B, T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            # crop idx to the last block_size tokens
+            idx_cond = idx[:, -block_size:]
+            # get the predictions
+            logits, loss = self(idx_cond)
+            # focus only on the last time step
+            logits = logits[:, -1, :] # becomes (B, C)
+            # apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1) # (B, C)
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+            # append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+        return idx
 
-
+model = LanguageModel()
+m = model.to(device)
 
