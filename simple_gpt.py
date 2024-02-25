8f54fef38ec35ea62eed32cbbf5995875b867601
PaulEm6 <paulemmanuelatte12@gmail.com>
2024-02-24 20:59:44 -0500

    implementation of simple gpt: embed, transformer (single head with self attention), output blocks

--- simple_gpt.py
+++ simple_gpt.py
@@ -67,21 +67,116 @@ def estimate_loss():
     model.train()
     return out
 
-'''Preparing Output'''
+'''Neural Network Architecture'''
 
-
-class LanguageModel(nn.Module):
+class EmbeddingBlock(nn.Module):
+    #Computing the token and positional embedding for each token in a block
     def __init__(self):
         super().__init__()
-        self.token_embedding_table = nn.Embedding(vocab_size, n_embed)
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embed) 
         self.position_embedding_table = nn.Embedding(block_size, n_embed)
 
     def forward(self, idx):
         B, T = idx.shape
-        token_embedding = self.token_embedding_table(idx)
-        positional_embedding = self.token_embedding_table(torch.arange(T, device=device))
-        x = token_embedding + positional_embedding
+        token_embedding = self.token_embedding_table(idx) #Output is (B, T, C)
+        positional_embedding = self.token_embedding_table(torch.arange(T, device=device)) #Output is (T, C)
+        x = token_embedding + positional_embedding #Output is (B, T, C)
         return x
+
+class AttentionBlock(nn.Module):
+    #Communication followed by computation i.e. Attention block followed by FeedForward MLP
+    #Head_size is the dimensino of the otuput of the key, query, value layers
+    #We do not use bias
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embed, head_size, bias=False)
+        self.query = nn.Linear(n_embed, head_size, bias=False)
+        self.value = nn.Linear(n_embed, head_size, bias=False)
+
+    def forward(self, x):
+        #Input size (Batch size, Time step, Channels)
+        #Output size (Batch size, Time step, Head size)
+
+        B, T, C = x.shape
+        k = self.key(x)
+        q = self.key(x)
+        
+        #Calculation of affinities between each token using the values from the key and query, "implementation of self attention" because current tokens
+        #Only get information from previous tokens (triangular inferior matrix)
+        wei = q @ k.transpose(-2,-1) * k.shape[-1]**-0.5 # (B, T, hs) @ (B, hs, T) -> (B, T, T)
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
+        wei = F.softmax(wei, dim=-1) # (B, T, T)
+        wei = self.dropout(wei)
+
+        #The input passes by another layer "value" and we multiply the tokens by the weighted averages
+        v = self.value(x) # (B,T,hs)
+        out = wei @ v # (B, T, T) @ (B, T, hs) -> (B, T, hs)
+        return out
+    
+class FeedForwardBlock(nn.Module):
+    #Multiple Layer Perceptron
+    def __init__(self, n_embed):
+        super().__init__()
+        # linear (input : n_embed, output: n_embed), relu, linear(input : n_embed, output: n_embed), dropout
+        self.MLP = nn.Sequential(
+            nn.Linear(n_embed, n_embed),
+            nn.ReLU(),
+            nn.Linear(n_embed, n_embed),
+            nn.Dropout(dropout),
+        )
+
+    def forward(self, x):
+        return self.MLP(x)
+        
+class TransformerBlock(nn.Module):
+    #Commmunication followed by computation i.e. Attention followed by Feed Forward block
+    #We add layer normalization and residual connections to improve results
+    def __init__(self, n_embed):
+        super().__init__()
+        self.AttentionBlock = AttentionBlock(n_embed)
+        self.FeedForwardBlock = FeedForwardBlock(n_embed)
+        self.ln1 = nn.LayerNorm(n_embed)
+        self.ln2 = nn.LayerNorm(n_embed)
+        
+    def forward(self, x):
+        x = x + self.ln1(self.AttentionBlock(x))
+        x = x + self.ln2(self.FeedForwardBlock(x))
+        return(x)
+
+class OutputBlock(nn.Module):
+    #Final output block contains layer normalization layer and linear (input: n_embed, output: vocab_size)
+    def __init__(self, n_embed):
+        super().__init__()
+        self.normalization_layer = nn.LayerNorm(n_embed)
+        self.linear_layer = nn.Linear(n_embed, vocab_size)
+        
+    def forward(self, x):
+        x = self.normalization_layer(x)
+        x = self.linear_layer(x)
+        return(x)
+
+class LanguageModel(nn.Module):
+    #We create the final model which contains the connection of the embedding transformer and output blocks
+    def __init__(self):
+        super().__init__()
+        self.EmbeddingBlock = EmbeddingBlock()
+        self.TransformerBlock = TransformerBlock(n_embed) 
+        self.OutputBlock = OutputBlock(n_embed)
+
+    def forward(self, idx, targets = None):
+        x = self.EmbeddingBlock(idx) #(B, T, C)
+        x = self.TransformerBlock(x) #(B, T, C)
+        logits = self.OutputBlock(x) #(B, T, Vocab size)
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
     
     def generate(self, idx, max_new_tokens):
     #For each block within the batch, this method generates the most probable token(s) to come next
@@ -104,4 +199,25 @@ class LanguageModel(nn.Module):
 
 model = LanguageModel()
 m = model.to(device)
+print("Training is using " + device)
+# print the number of parameters in the model
+print(sum(p.numel() for p in m.parameters())/1e6, 'M parameters')
 
+# create a PyTorch optimizer
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+
+for iter in range(max_iters):
+
+    # every once in a while evaluate the loss on train and val sets
+    if iter % eval_interval == 0 or iter == max_iters - 1:
+        losses = estimate_loss()
+        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+
+    # sample a batch of data
+    xb, yb = get_batch('train')
+
+    # evaluate the loss
+    logits, loss = model(xb, yb)
+    optimizer.zero_grad(set_to_none=True) #Set the gradient to zero
+    loss.backward() #Apply back propagation
+    optimizer.step() #Update the parameters of the model
