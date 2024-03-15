fd7998c25b5e5f59a69741534968d72f612a75ee
PaulEm6 <paulemmanuelatte12@gmail.com>
2024-03-15 15:01:15 -0400

    tokenizer works with sentence piece

--- Tokenization/test_token.py
+++ Tokenization/test_token.py
@@ -1,8 +1,5 @@
-#https://github.com/google/sentencepiece/blob/master/doc/experiments.md
-
 import sentencepiece as spm
 import os
-import sys
 
 # Example corpus
 corpus = ["Hello, how are you?", "I am fine, thank you."]
@@ -17,21 +14,8 @@ with open(corpus_file_path, "w") as f:
 if not os.path.exists(corpus_file_path):
     raise FileNotFoundError(f"The corpus file '{corpus_file_path}' does not exist.")
 
-# Save original stdout and stderr
-original_stdout = sys.stdout
-original_stderr = sys.stderr
-
-# Redirect stdout and stderr to /dev/null
-with open(os.devnull, 'w') as devnull:
-    sys.stdout = devnull
-    sys.stderr = devnull
-
-    # Train SentencePiece model with BPE
-    spm.SentencePieceTrainer.Train(input=corpus_file_path, model_prefix='spm_model', vocab_size=26, model_type='bpe')
-
-# Restore original stdout and stderr
-sys.stdout = original_stdout
-sys.stderr = original_stderr
+# Train SentencePiece model with BPE
+spm.SentencePieceTrainer.Train(input=corpus_file_path, model_prefix='spm_model', vocab_size=26, model_type='bpe', minloglevel = 2)
 
 sp = spm.SentencePieceProcessor()
 sp.Load(model_file='spm_model.model')
