7f87a1a6e51ad0cee46337bc235893e04f0dc47f
PaulEm6 <paulemmanuelatte12@gmail.com>
2024-03-19 20:36:39 -0400

    SENTENCEPIECE: VERSION ALPHA (without multithreading and architecture review)

--- Training Scripts/simple_gpt.py
+++ Training Scripts/simple_gpt.py
@@ -6,11 +6,11 @@ import sentencepiece as spm
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 '''Hyperparameters'''
-batch_size = 32 # how many independent sequences will we process in parallel?
-block_size = 8 #maximum number of tokens to be considered as "input" for predictions
+batch_size = 64 # how many independent sequences will we process in parallel?
+block_size = 256 #maximum number of tokens to be considered as "input" for predictions
 n_embed = 32 #dimension of vector after embedding
 n_blocks = 6 #number of sequential attention blocks
-vocab_size = 65 #Dictionary size i.e. amount of possible sub words tokens to be created from dataset 
+vocab_size = 4000 #Dictionary size i.e. amount of possible sub words tokens to be created from dataset 
 
 max_iters = 5000
 eval_interval = 500
@@ -36,8 +36,6 @@ sp.Load(model_file='spm_model.model')
 
 # Train and test splits
 data = torch.tensor(sp.Encode(text, out_type=int), dtype=torch.long)
-
-
 
 n = int(0.9*len(data)) # first 90% will be train, rest val
 train_data = data[:n]
